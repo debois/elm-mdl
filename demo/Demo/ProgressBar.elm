f50a297ca8a3cdf3fc3516d572c530e80ac1d54f
Ville Penttinen <villem.penttinen@gmail.com>
2016-06-14 19:55:38 +0300

    Add simulated loading with a progress bar

--- demo/Demo/ProgressBar.elm
+++ demo/Demo/ProgressBar.elm
@@ -5,9 +5,14 @@ import Html exposing (..)
 
 import Material.ProgressBar as ProgressBar
 import Material
+import Material.Options as Options
 
 import Material.Grid as Grid
 import Demo.Page as Page
+
+import Material.Helpers as Helpers
+
+import Material.Button as Button
 
 
 
@@ -20,12 +25,16 @@ type alias Mdl =
 
 type alias Model =
   { mdl : Material.Model
+  , started : Bool
+  , currentProgress : Float
   }
 
 
 model : Model
 model =
   { mdl = Material.model
+  , started = False
+  , currentProgress = 0
   }
 
 
@@ -33,15 +42,38 @@ model =
 
 
 type Msg
-  = TemplateMsg
+  = Tick
   | Mdl Material.Msg
+  | Start
 
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update action model =
   case action of
-    TemplateMsg ->
-      (model, Cmd.none)
+      -- 'Simulate' a process that takes some time
+    Tick ->
+      let
+        nextProgress = model.currentProgress + 1
+        progress = if nextProgress > 100 then
+                     0
+                   else
+                     nextProgress
+
+        finishedLoading = nextProgress > 100
+
+        command = if not finishedLoading then
+                    Helpers.delay 100 Tick
+                  else
+                    Cmd.none
+      in
+        ({ model | currentProgress = progress
+         , started = not finishedLoading }, command)
+
+    Start ->
+      if model.started then
+        (model, Cmd.none)
+      else
+        ({ model | started = True}, Helpers.delay 200 Tick)
 
     Mdl action' ->
       Material.update Mdl action' model
@@ -52,7 +84,7 @@ update action model =
 
 view : Model -> Html Msg
 view model  =
-  Page.body2 "ProgressBar" srcUrl intro references
+  Page.body2 "Progress bars" srcUrl intro references
     [
      (Grid.grid []
         [ Grid.cell
@@ -71,11 +103,28 @@ view model  =
 
         , Grid.cell
             [ Grid.size Grid.All 4]
-            [ ProgressBar.render Mdl [1] model.mdl
+            [ ProgressBar.render Mdl [2] model.mdl
                 [ ProgressBar.buffer 78
                 , ProgressBar.progress 44]
                 []
             , p [] [text "Buffering "]
+            ]
+
+        , Grid.cell
+            [ Grid.size Grid.All 4]
+            [ ProgressBar.render Mdl [3] model.mdl
+                [ ProgressBar.progress model.currentProgress ]
+                []
+            , p [] [text "Loading bar with progress update"]
+            , div []
+              [ Button.render Mdl [4] model.mdl
+                  [ Button.raised
+                  , Button.colored
+                  , Button.ripple
+                  , if model.started then Button.disabled else Options.nop
+                  , Button.onClick (Start)]
+                  [text "Start loading"]
+              ]
             ]
         ])
     ]
@@ -90,12 +139,13 @@ intro =
 > conveys a sense of motion. While some progress devices indicate an approximate
 > or specific percentage of completion, the MDL progress component simply
 > communicates the fact that an activity is ongoing and is not yet complete.
-
+>
 > Progress indicators are an established but non-standardized feature in user
 > interfaces, and provide users with a visual clue to an application's status.
 > Their design and use is therefore an important factor in the overall user
 > experience. See the progress component's Material Design specifications page for
 > details.
+
 """
 
 
