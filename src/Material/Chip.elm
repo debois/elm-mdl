aforemny/elm-mdc

commit 98f4284907e9fac7b4dd4a47a6abfc627f38b686

Switch to Options.on* where applicable.

src/Material/Chip.elm
@@ -39,7 +39,6 @@ for a live demo.
 -}
 
 import Html exposing (Attribute, Html)
-import Html.Events
 import Html.Attributes
 import Material.Options as Options exposing (cs)
 import Material.Icon as Icon
@@ -103,24 +102,15 @@ deleteLink =
 
 {-| Set the `onClick` for the delete action
 
-NOTE. This stops propagation and prevents default to stop `Chip.onClick` from being called
-when this is clicked
+NOTE. This stops propagation and prevents default to stop `onClick` from being
+called when this is clicked.
 -}
 deleteClick : msg -> Property msg
-deleteClick =
-    -- We want to prevent the onClick of the Chip itself being called when clicking on the delete action
-    Internal.option
-        << (\msg config ->
-                { config
-                    | deleteClick =
-                        Just
-                            (Html.Events.onWithOptions "click"
-                                { stopPropagation = True, preventDefault = True }
-                                (Json.succeed msg)
-                            )
-                }
-           )
-
+deleteClick msg =
+    Options.onWithOptions
+      "click" 
+       { stopPropagation = True, preventDefault = True }
+       (Json.succeed msg)
 
 type alias Priority =
     Int

demo/Demo/Lists.elm
@@ -2,7 +2,6 @@ module Demo.Lists exposing (..)
 
 import Platform.Cmd exposing (Cmd, none)
 import Html exposing (..)
-import Html.Events 
 import Set exposing (Set)
 import String
 
@@ -101,17 +100,17 @@ click model =
     [ Lists.ul [ css "margin" "0", css "padding" "0" ]
         [ Lists.li [] 
             [ Lists.content 
-                [ Options.attribute <| Html.Events.onClick (Click "Elm") ] 
+                [ Options.onClick (Click "Elm") ] 
                 [ text "Elm" ] 
             ]
         , Lists.li [] 
             [ Lists.content 
-                [ Options.attribute <| Html.Events.onClick (Click "F#") ] 
+                [ Options.onClick (Click "F#") ] 
                 [ text "F#" ] 
             ]
         , Lists.li [] 
             [ Lists.content 
-                [ Options.attribute <| Html.Events.onClick (Click "Lisp") ] 
+                [ Options.onClick (Click "Lisp") ] 
                 [ text "Lisp" ] 
             ]
         ]

src/Material.elm
@@ -68,7 +68,7 @@ by supplying these `Property m`, much the same way you set attributes of
 NB! If you are using parts to suppress TEA boilerplate, call `render` instead
 of `view` (see below).
 
-# Suppressing TEA boilerplate
+# Shorthands for TEA components
 
 The component model of the library is simply the Elm Architecture (TEA), i.e.,
 each component has types `Model` and `Msg`, and values `view` and `update`. A
@@ -76,14 +76,15 @@ minimal example using this library as plain TEA can be found
 [here](https://github.com/debois/elm-mdl/blob/master/examples/Component-TEA.elm).
 
 Using more than a few component in plain TEA is unwieldy because of the large
-amount of boilerplate one has to write. This library uses the
-[Parts mechanic](https://github.com/debois/elm-parts) suppress most of
-  that boilerplate. A minimal example using parts is
-[here](http://github.com/debois/elm-mdl/blob/master/examples/Component.elm).
+amount of boilerplate one has to write. The elm-mdl library supports
+shorthands for avoiding most of this boilerplate. A minimal example using
+shorthands is
+  [here](http://github.com/debois/elm-mdl/blob/master/examples/Component.elm).
 
-It is important to note that parts is not an alternative to TEA; it is simply
-a library which hides much of the tedious TEA boilerplate.
-
+It is important to note that the shorthands are not an alternative to TEA; they
+simply do much of the tedious TEA boilerplate. Note that elm-mdl no longer
+depends on the [Parts library](https://github.com/debois/elm-parts) and do not
+put functions in messages. 
 
 ## Required boilerplate
 
@@ -93,7 +94,7 @@ with component support can be found
 [here](http://github.com/debois/elm-mdl/blob/master/examples/Component.elm).
 We encourage you to use the library in this fashion.
 
-Here is how you use elm-mdl with parts. First, boilerplate.
+Here is how you use elm-mdl with shorthands. First, boilerplate.
 
  1. Add a model container for Material components to your model:
 
@@ -118,8 +119,8 @@ Here is how you use elm-mdl with parts. First, boilerplate.
         update message model =
           case message of
             ...
-            Mdl message' ->
-              Material.update message' model
+            Mdl message_ ->
+              Material.update Mdl message_ model
 
  4.  If your app is using Layout and/or Menu, you need also to set up
  subscriptions and initialisations; see `subscriptions` and `init` below.
@@ -129,6 +130,7 @@ Let's say you need a textfield for name entry, and you'd like to be notifed
 whenever the field changes value through your own NameChanged action:
 
         import Material.Textfield as Textfield
+        import Material.Options as Options
 
         ...
 
@@ -140,7 +142,7 @@ whenever the field changes value through your own NameChanged action:
           Textfield.render [0] Mdl model.mdl
             [ css "width" "16rem"
             , Textfield.floatingLabel
-            , Textfield.onInput NameChanged
+            , Options.onInput NameChanged
             ]
 
 The win relative to using plain Elm Architecture is that adding a component
@@ -151,13 +153,14 @@ neither requires you to update your model, your Msgs, nor your update function.
 
 Using this module will force all elm-mdl components to be built and included in
 your application. If this is unacceptable, you can custom-build a version of this
-module that exposing uses only the components you need. To do so, you need to provide your
-own versions of the type `Model` and the value `model` of the present module.
-Use the corresponding definitions in this module as a starting point
-([source](https://github.com/debois/elm-mdl/blob/master/src/Material.elm))
-and simply comment out the components you do not need.
+module that exposing uses only the components you need. To do so, you need to
+provide your own versions of the type aliases `Msg` and `Model` and the value
+`model` of the present module.  Use the corresponding definitions in this
+module as a starting point
+  ([source](https://github.com/debois/elm-mdl/blob/master/src/Material.elm))
+  and simply comment out the components you do not need.
 
-## Parts API
+## Shorthands
 
 @docs Model, model, Msg, Container, update, subscriptions, init
 -}
@@ -283,14 +286,14 @@ follows.
 
     type Msg =
       ...
-      | Mdl Material.Msg
+      | Mdl (Material.Msg Msg)
 
     ...
 
     App.program
       { init = ( model, Material.init Mdl )
       , view = view
-      , subscriptions = Material.subscriptions Mdl
+      , subscriptions = Material.subscriptions Mdl 
       , update = update
       }
 

src/Material/Layout.elm
@@ -769,6 +769,8 @@ drawerButton lift isVisible =
                     "false"
                 )
             , tabindex 1
+            {- No-one else is putting events on the drawerbutton, so we don't 
+            need to go through dispatch here. -}
             , Events.onClick (lift ToggleDrawer)
             , Events.onWithOptions
                 "keydown"

src/Material/Menu.elm
@@ -591,9 +591,13 @@ view lift model properties items =
                 [ cs "mdl-button"
                 , cs "mdl-js-button"
                 , cs "mdl-button--icon"
-                , when (isActive model) (Internal.attribute (onKeyDown (Key itemSummaries)))
-                , when (model.animationState /= Opened) (Internal.attribute (onClick Geometry.decode (Open)))
-                , when (isActive model) (Internal.attribute (Html.Events.onClick Close))
+                , when 
+                    (isActive model) 
+                    (onKeyDown (Key itemSummaries))
+                , when 
+                    (model.animationState /= Opened) 
+                    (Options.on "click" (Json.map Open Geometry.decode))
+                , when (isActive model) (Options.onClick Close)
                 ]
                 [ Icon.view config.icon
                     [ cs "material-icons"
@@ -814,9 +818,9 @@ onClick decoder action =
     Html.Events.on "click" (Json.map action decoder)
 
 
-onKeyDown : (Int -> m) -> Attribute m
+onKeyDown : (Int -> m) -> Options.Property c m
 onKeyDown action =
-    Html.Events.onWithOptions
+    Options.onWithOptions
         "keydown"
         { preventDefault = True
         , stopPropagation = False

src/Material/Options.elm
@@ -512,7 +512,7 @@ onWithOptions evt options =
 -- DISPATCH
 
 
-{-| Plain-TEA multiple-event dispatch.
+{-| No-shorthand multiple-event dispatch.
 
 NB! You are _extremely_ unlikely to need this.
 
@@ -529,7 +529,7 @@ same event as your custom handler; e.g., you install `onBlur` on
 In this case you need to tell the component how to dispatch multiple messages
 (one for you, one for itself) in response to a single DOM event. You do so by
 providing a means of folding a list of messages into a single message. (See
-the [Dispatch](TODO) library for one way to define such a function.)
+the [Dispatch](https://github.com/vipentti/elm-dispatch) library for one way to define such a function.)
 
 The `render` function does all this automatically. If you are calling `render`,
 you do not need this property.

src/Material/Textfield.elm
@@ -454,7 +454,7 @@ react =
       [ Textfield.label "Age"
       , Textfield.floatingLabel
       , Textfield.value model.age
-      , Textfield.onInput (String.toInt >> ChangeAgeMsg)
+      , Options.onInput (String.toInt >> ChangeAgeMsg)
       ]
 
 Be aware that styling (third argument) is applied to the outermost element

debois/elm-mdl

commit 7c2565d15c3f091d6565f7f96ef6eea5dd94d4f5

Fix event handling of deleteClick
Regression introduced by commit 98f4284907e9fac. Model's deleteClick
was not being filled out. Instead of that, the message intended for
deleteClick was overriding the one intended for onClick.

src/Material/Chip.elm
@@ -40,6 +40,7 @@ for a live demo.
 
 import Html exposing (Attribute, Html)
 import Html.Attributes
+import Html.Events
 import Material.Options as Options exposing (cs)
 import Material.Icon as Icon
 import Material.Options.Internal as Internal
@@ -106,11 +107,18 @@ NOTE. This stops propagation and prevents default to stop `onClick` from being
 called when this is clicked.
 -}
 deleteClick : msg -> Property msg
-deleteClick msg =
-    Options.onWithOptions
-      "click" 
-       { stopPropagation = True, preventDefault = True }
-       (Json.succeed msg)
+deleteClick =
+    Internal.option
+        << (\msg config ->
+                { config
+                    | deleteClick =
+                        Just
+                            (Html.Events.onWithOptions "click"
+                                { stopPropagation = True, preventDefault = True }
+                                (Json.succeed msg)
+                            )
+                }
+           )
 
 type alias Priority =
     Int